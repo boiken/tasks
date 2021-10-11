a21f85284299af3f15849c53827e18c620b6d43b
boiken <77499247+boiken@users.noreply.github.com>
2021-10-11 12:18:01 +0200

    Update create_3vms.tf

--- question1/create_3vms.tf
+++ question1/create_3vms.tf
@@ -4,17 +4,17 @@ provider "aws" {
   secret_key = "CASASMCJSJ(MJF(ASFAM)S(MFASFM))"
 }
 
-resource "aws_instance" "VM1" {
+resource "aws_instance" "Master" {
+  ami = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "Worker1" {
   ami = "ami-02e136e904f3da870"
   instance_type = "t2.medium"
 }
 
-resource "aws_instance" "VM2" {
-  ami = "ami-02e136e904f3da870"
-  instance_type = "t2.medium"
-}
-
-resource "aws_instance" "VM3" {
+resource "aws_instance" "Worker2" {
   ami = "ami-02e136e904f3da870"
   instance_type = "t2.medium"
 }
